861675c1cfcb8aa606011eecf0104c986db93650
figueiredodavi <99026991+figueiredoUm@users.noreply.github.com>
2023-07-25 13:00:50 -0300

    Forgot to ctlr+s (i hate git)

--- index.js
+++ index.js
@@ -9,13 +9,12 @@ const score = JSON.parse(localStorage.getItem('score')) || {
 updateScoreElement();
 
 
-console.log(score)
 
-function play(n) {
-  playerChoice = n;
+function play(number) {
+  playerChoice = number;
   computerChoice = Math.floor(Math.random() * 3 + 1);
-
-  document.querySelector("#computer").innerHTML = htmlCmpChoice();
+  displayChoiceAnimation(number);
+  displayCmpChoice(computerChoice);
 
   if (computerChoice == playerChoice) {
     tie();
@@ -63,16 +62,47 @@ function tie(){
   document.querySelector('#ties').innerHTML = ++score.ties;
 }
 
-function htmlCmpChoice() {
-  switch(computerChoice) {
+function displayCmpChoice(number) {
+  switch(number) {
     case 1:
-      return "Rock";
+      document.querySelector('.computer').innerHTML = '<img class = "img"  src = "images/rock.png">'
+      break;
     case 2:
-      return "Paper";
+      document.querySelector('.computer').innerHTML = '<img class = "img"  src = "images/paper.png">'
+      break;
     case 3:
-      return "Scissors";
+      document.querySelector('.computer').innerHTML = '<img class = "img" src = "images/scissors.png">'
+      break;
   }
 }
+
+/*
+function displayPlayerChoice(number) {
+  let selected, nonSelected1, nonSelected2;
+  switch(number) {
+    case 1:
+      selected = 'rock';
+      nonSelected1 = 'paper';
+      nonSelected2 = 'scissors'
+      break;
+    case 2:
+      selected = 'paper';
+      nonSelected1 = 'rock';
+      nonSelected2 = 'scissors'
+      break;
+    case 3:
+      selected = 'scissors';
+      nonSelected1 = 'rock';
+      nonSelected2 = 'paper'
+      break;
+  }
+  document.getElementById(selected).style.pointerEvents = 'none';
+  document.getElementById(nonSelected1).innerHTML = '';
+  document.getElementById(nonSelected2).innerHTML = '';
+
+  document.querySelector('.buttons').style.gap = '0';
+}
+*/
 
 function clearResult() {
   score.wins = 0;
@@ -88,4 +118,57 @@ function updateScoreElement() {
   document.querySelector('#ties').innerHTML = score.ties;
 }
 
+/* Animation Test */
+
+function displayChoiceAnimation(number){
+  let selected, nonSelected1, nonSelected2;
+  if(number === 1) {
+    selected = 'rock';
+  } else if (number === 2) {
+    selected = 'paper';
+  } else {
+    selected = 'scissors';
+  }
+
+  switch(selected) {
+    case 'rock':
+      nonSelected1 = 'paper';
+      nonSelected2 = 'scissors';
+      document.getElementById(selected).style.pointerEvents = 'none';
+      document.getElementById(nonSelected1).style.opacity = '0';
+      document.getElementById(nonSelected2).style.opacity = '0';
+      document.getElementById(selected).classList.toggle('animate-rock');
+
+      setTimeout(function(){
+        document.getElementById(nonSelected1).innerHTML = '';
+        document.getElementById(nonSelected2).innerHTML = '';
+        document.querySelector('.buttons').style.gap = '0';
+      }, 1000);
+      break;
+
+    case 'paper':
+      nonSelected1 = 'rock';
+      nonSelected2 = 'scissors';
+      document.getElementById(selected).style.pointerEvents = 'none';
+      document.getElementById(nonSelected1).innerHTML = '';
+      document.getElementById(nonSelected2).innerHTML = '';
+      document.querySelector('.buttons').style.gap = '0';
+      break;
+    
+    case 'scissors':
+      nonSelected1 = 'rock';
+      nonSelected2 = 'paper';
+      document.getElementById(selected).style.pointerEvents = 'none';
+      document.getElementById(nonSelected1).style.opacity = '0';
+      document.getElementById(nonSelected2).style.opacity = '0';
+      document.getElementById(selected).classList.toggle('animate-scissors');
+
+      setTimeout(function(){
+        document.querySelector('.buttons').style.gap = '0';
+        document.getElementById(nonSelected1).innerHTML = '';
+        document.getElementById(nonSelected2).innerHTML = '';
+      }, 1000);
+      break;
+  }
+}
 
